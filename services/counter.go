eba975937610806d1cf1439409d94a5508062c5c
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-10-30 18:31:19 -0300

    introduce counter service

---
+++ services/counter.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Counter(filename string, message string, split_func bufio.SplitFunc) error {
+	file, err := os.Open(filename)
+	defer file.Close()
+
+  if err != nil { return fmt.Errorf("Error when open file %s: %v", filename, err) }
+
+  scanner := bufio.NewScanner(file)
+  scanner.Split(split_func)
+
+  counter := 0
+
+  for scanner.Scan() { counter++ }
+  fmt.Println(message, counter)
+
+  return nil
+}
