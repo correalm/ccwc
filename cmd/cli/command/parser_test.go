c3f86730d830f49c9b4801b1e1595ec78bfaec5a
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-10-31 08:43:12 -0300

    test call of helper function when no args are passed

---
+++ cmd/cli/command/parser_test.go
@@ -0,0 +1,37 @@
+package command
+
+import (
+	"ccwc/internal/interfaces"
+	"testing"
+)
+
+type Helper interface {
+  Help()
+}
+
+type SpyHelper struct {
+  Calls int
+}
+
+func (s *SpyHelper) Call() {
+  s.Calls++
+}
+func TestHelpCallWithNoArgs(t *testing.T) {
+  parser := &Parser{
+    commands: []interfaces.Command{
+      NewCountLines(""),
+    },
+  }
+
+  spy := &SpyHelper{}
+
+  err := parser.Parse([]string{}, spy.Call)
+
+  if err != nil {
+    t.Errorf("parser.Parse() error: %v", err)
+  }
+
+  if spy.Calls < 1 {
+    t.Errorf("help function not called")
+  }
+}
