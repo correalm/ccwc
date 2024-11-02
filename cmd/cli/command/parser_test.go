5aabce8e6dab3ec7cdfc57f070faa6448ee73fa4
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-11-02 18:49:51 -0300

    specs for parser (test individual args)

--- cmd/cli/command/parser_test.go
+++ cmd/cli/command/parser_test.go
@@ -1,7 +1,10 @@
-package command
+package command_test
 
 import (
+	"bytes"
+	"ccwc/cmd/cli/command"
 	"ccwc/internal/interfaces"
+	"os"
 	"testing"
 )
 
@@ -16,12 +19,103 @@ type SpyHelper struct {
 func (s *SpyHelper) Call() {
   s.Calls++
 }
-func TestHelpCallWithNoArgs(t *testing.T) {
-  parser := &Parser{
-    commands: []interfaces.Command{
-      NewCountLines(""),
+
+const FILENAME = "../../../__test__/fixtures/test.txt"
+
+func TestParser(t *testing.T) {
+  type fields struct {
+    commands []interfaces.Command
+  }
+  type args struct {
+    args []string
+  }
+
+  tests := []struct {
+    name string
+    fields fields
+    args args
+    wantError bool
+    helper func()
+    expected string
+  } {
+    {
+      name: "returns the number of lines when l arg is passed",
+      fields: fields{
+        commands: []interfaces.Command{
+          command.NewCountLines(FILENAME),
+        },
+      },
+      args: args{
+        args: []string{"-l"},
+      },
+      wantError: false,
+      helper: func() {},
+      expected: "Lines: 7145\n",
+    },
+    {
+      name: "returns the number of words when w arg is passed",
+      fields: fields{
+        commands: []interfaces.Command{
+          command.NewCountWords(FILENAME),
+        },
+      },
+      args: args{
+        args: []string{"-w"},
+      },
+      wantError: false,
+      helper: func() {},
+      expected: "Words: 58164\n",
+    },
+    {
+      name: "returns the number of bytes when b arg is passed",
+      fields: fields{
+        commands: []interfaces.Command{
+          command.NewByteCounter(FILENAME),
+        },
+      },
+      args: args{
+        args: []string{"-b"},
+      },
+      wantError: false,
+      helper: func() {},
+      expected: "Bytes: 342190\n",
     },
   }
+
+  for _, test := range tests {
+    t.Run(test.name, func(t *testing.T) {
+      parser := command.NewParser(test.fields.commands)
+
+      originalOut := os.Stdout
+
+      r, w, _ := os.Pipe()
+      os.Stdout = w
+
+      err := parser.Parse(test.args.args, test.helper)
+
+      w.Close()
+      var buf bytes.Buffer
+      buf.ReadFrom(r)
+
+      os.Stdout = originalOut
+
+      result := buf.String()
+
+      if err != nil && !test.wantError {
+        t.Errorf("parser.Parse() error: %v", err)
+      }
+
+      if result != test.expected {
+        t.Errorf("Expected: %v; Actual: %v", test.expected, result)
+      }
+    })
+  }
+}
+
+func TestHelpCallWithNoArgs(t *testing.T) {
+  parser := command.NewParser([]interfaces.Command{
+    command.NewCountLines(""),
+  })
 
   spy := &SpyHelper{}
 
@@ -37,11 +131,9 @@ func TestHelpCallWithNoArgs(t *testing.T) {
 }
 
 func TestHelpCallWithArgs(t *testing.T) {
-  parser := &Parser{
-    commands: []interfaces.Command{
-      NewCountLines(""),
-    },
-  }
+  parser := command.NewParser([]interfaces.Command{
+    command.NewCountLines(""),
+  })
 
   spy := &SpyHelper{}
 
