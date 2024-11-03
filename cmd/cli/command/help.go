b6dedcd7a96a6cdf823119560b4d2de5d24ffaa7
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-11-03 13:25:26 -0300

    help implements Command interface

--- cmd/cli/command/help.go
+++ cmd/cli/command/help.go
@@ -1,6 +1,39 @@
 package command
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+type Helper struct {
+  fs *flag.FlagSet
+}
+
+func NewHelp() *Helper {
+  cmd := &Helper{
+    fs: flag.NewFlagSet("-h", flag.ContinueOnError),
+  }
+
+  return cmd
+}
+
+func (cmd *Helper) Name() string {
+  return cmd.fs.Name()
+}
+
+func (cmd *Helper) ParseFlags(flags []string) error {
+  if len(flags) == 0 {
+    return fmt.Errorf("missing flags")
+  }
+
+  return cmd.fs.Parse(flags)
+}
+
+func (cmd *Helper) Run() error {
+  Help()
+
+  return nil
+}
 
 func Help() {
 	help := `usage: ./ccwc [<flags>] filename
