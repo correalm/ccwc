3b9d1914a558f54675e438f1ca87ed399fa2a507
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-11-03 12:47:31 -0300

    improve help message

--- cmd/cli/command/help.go
+++ cmd/cli/command/help.go
@@ -3,6 +3,17 @@ package command
 import "fmt"
 
 func Help() {
-	help := "usage: [<flags>] filename"
+	help := `usage: ./ccwc [<flags>] filename
+
+  Available commands:
+  -l print the number of lines
+  -b print the number of bytes
+  -w print the number of words
+
+  More than one command:
+  ./ccwc -l -w filename
+
+  When no flags are provided, the program will print all information
+`
 	fmt.Println(help)
 }
